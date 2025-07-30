816375ee9f390f1aec568098100471457ed96ad6
piyush <piyush.khadka2233@gmail.com>
2025-07-30 11:10:55 +0100

    password saver

---
+++ app.py
@@ -0,0 +1,8 @@
+#password collector
+
+email_used = input("Enter your email")
+used_password = input("Enter your password:")
+username_used = input("Enter your username:")
+genre_site = input("Job, Game, Social, Mesc")
+
+
